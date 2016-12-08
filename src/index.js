445bd1a4b6f740d546c9630bbd73c79269058c2e
yzy <yzy@yuanzimao.com>
2016-12-08 20:24:43 +0800

    目前页面更改基本完毕，等待换图等，还差一些，滚动略有bug 导航栏没有和应该到底的位子对齐

--- src/index.js
+++ src/index.js
@@ -4,3 +4,48 @@ window.onresize=function(){
     var nowh = document.body.clientHeight;
     console.log(noww,nowh)
 }
+
+//当页面加载完毕的时候
+$(function(){
+
+    //注册点击事件实现页面下滑
+    $('.btn-list>li>a').click(function(){
+        var headerHeight=$('.header').height(),
+            bannerListHeight=$('.banner-list').height(),
+            allHeight=headerHeight+bannerListHeight;
+
+        $('.btn-list>li>a').removeClass('font-current');
+
+        $(this).addClass('font-current');
+
+        var index=$(this).attr('index');
+
+        $('html,body').animate({scrollTop:$("#link_"+index+"").offset().top-headerHeight},500);
+
+    })
+
+    //通过滚动事件实现头部和导航栏的重合
+    $(window).scroll(function() {
+        //让我们先获取头部和导航栏的高度吧 哈哈
+        var headerHeight=$('.header').height(),
+            bannerListHeight=$('.banner-list').height(),
+            allHeight=headerHeight+bannerListHeight,
+            topHeight=$(document).scrollTop();
+       //现在让我们做个判断吧
+        if(topHeight==0){
+            $('.header').slideDown(1200);
+            $('#tag-logo').css('display','none');
+        }
+        else{
+            $('.header').slideUp(1200);
+            $('#tag-logo').slideDown(1200);
+        }
+
+
+    });
+
+    //导航栏固定定位
+    $(".banner-list").sticky({topSpacing:0});
+    console.log($('.header').height());
+
+})
